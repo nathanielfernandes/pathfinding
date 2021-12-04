d833b77baadf59859123be6dc9262283c82b029e
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-12-03 19:17:12 -0500

    works just needs a clean

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,10 @@ impl Direction {
 
     pub fn delta_dirs() -> Iter<'static, (f32, f32)> {
         [
-            (0.0, -SQUARE_SCALE),
             (SQUARE_SCALE, 0.0),
-            (0.0, SQUARE_SCALE),
+            (0.0, -SQUARE_SCALE),
             (-SQUARE_SCALE, 0.0),
+            (0.0, SQUARE_SCALE),
         ]
         .iter()
     }
@@ -81,7 +81,6 @@ struct Arena {
     pub greatest_weight: f32,
     pub found_objective: Option<usize>,
     pub open: Vec<usize>,
-    pub closed: Vec<usize>,
 }
 
 impl Arena {
@@ -92,7 +91,6 @@ impl Arena {
             greatest_weight: 0.0,
             found_objective: None,
             open: vec![0],
-            closed: Vec::new(),
         }
     }
     pub fn new_node(&mut self, pos: (f32, f32)) -> usize {
@@ -102,23 +100,25 @@ impl Arena {
             parent: None,
             index: next_index,
             pos,
-            weight: 0.0,
+            cost: 0.0,
+            priority: 0.0,
         });
 
         next_index
     }
 
-    pub fn new_child(&mut self, parent: usize, pos: (f32, f32), weight: f32) -> usize {
+    pub fn new_child(&mut self, parent: usize, pos: (f32, f32), cost: f32, priority: f32) -> usize {
         let next_index = self.nodes.len();
 
-        if weight as f32 > self.greatest_weight {
-            self.greatest_weight = weight as f32
+        if cost as f32 > self.greatest_weight {
+            self.greatest_weight = cost as f32
         }
         self.nodes.push(Node {
             parent: Some(parent),
             index: next_index,
             pos,
-            weight,
+            cost,
+            priority,
         });
 
         next_index
@@ -127,7 +127,7 @@ impl Arena {
     pub fn draw(&self) {
         self.nodes.iter().for_each(|n| {
             let (x, y) = n.pos;
-            let r = n.weight / self.greatest_weight;
+            let r = n.cost / self.greatest_weight;
             draw_poly(
                 x,
                 y,
@@ -160,11 +160,11 @@ impl Arena {
                 let node = &self.nodes[i];
                 let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
 
-                if !self.exists(new_pos) && !walls.exists(&new_pos) {
+                if self.exists(new_pos).is_none() && !walls.exists(&new_pos) {
                     let mut weight: usize = 0;
                     node.total_steps(&mut weight, self);
 
-                    just_searched.push(self.new_child(i, new_pos, weight as f32));
+                    just_searched.push(self.new_child(i, new_pos, weight as f32, 0.0));
 
                     if &new_pos == objective {
                         self.found_objective = Some(self.nodes.len() - 1);
@@ -191,8 +191,8 @@ impl Arena {
             .into_iter()
             .min_by(|n1, n2| {
                 self.nodes[*n1]
-                    .weight
-                    .partial_cmp(&self.nodes[*n2].weight)
+                    .cost
+                    .partial_cmp(&self.nodes[*n2].cost)
                     .unwrap()
             })
             .unwrap();
@@ -205,9 +205,9 @@ impl Arena {
         for (dx, dy) in Direction::delta_dirs() {
             let node = &self.nodes[i];
             let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-            if !self.exists(new_pos) && !walls.exists(&new_pos) {
+            if self.exists(new_pos).is_none() && !walls.exists(&new_pos) {
                 let weight = Arena::heuristic(*objective, new_pos);
-                just_searched.push(self.new_child(i, new_pos, weight));
+                just_searched.push(self.new_child(i, new_pos, weight, 0.0));
 
                 if &new_pos == objective {
                     self.found_objective = Some(self.nodes.len() - 1);
@@ -227,43 +227,69 @@ impl Arena {
         objective: &(f32, f32),
         _instant: &bool,
     ) -> bool {
+        //let mut sorted = self.open.clone();
         if self.open.is_empty() {
             return true;
         }
+
         let i = self
             .open
             .clone()
             .into_iter()
             .min_by(|n1, n2| {
                 self.nodes[*n1]
-                    .weight
-                    .partial_cmp(&self.nodes[*n2].weight)
+                    .priority
+                    .partial_cmp(&self.nodes[*n2].priority)
                     .unwrap()
             })
             .unwrap();
 
+        // sorted.sort_by(|n1, n2| {
+        //     self.nodes[*n1]
+        //         .weight
+        //         .partial_cmp(&self.nodes[*n2].weight)
+        //         .unwrap()
+        // });
+
+        // for i in sorted {
         let p = self.open.iter().position(|u| u == &i).unwrap();
         self.open.remove(p);
 
         let mut just_searched: Vec<usize> = Vec::new();
+        //self.open.remove(self.open.iter().position(|u| u ))
 
         for (dx, dy) in Direction::delta_dirs() {
             let node = &self.nodes[i];
             let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
+            //let mut steps = 0;
 
-            let mut steps = 1;
-            node.total_steps(&mut steps, self);
-            let weight = ((steps as f32 + 1.0) * 20.0) + Arena::heuristic(*objective, new_pos);
+            //node.total_steps(&mut steps, self);
+            let new_cost = node.cost + SQUARE_SCALE;
 
-            if !self.exists(new_pos)
-                && !walls.exists(&new_pos)
-                && (weight < node.weight || node.parent.is_none())
-            {
+            let prev_exp = self.exists(new_pos);
+
+            let is_none = prev_exp.is_none();
+
+            //((steps as f32 - 1.0) * SQUARE_SCALE) + Arena::heuristic(*objective, new_pos);
+
+            //println!("{}", weight);
+            if (is_none || new_cost < prev_exp.unwrap().cost) && !walls.exists(&new_pos) {
                 //let heuristic =
 
+                let weight = new_cost + Arena::heuristic(new_pos, *objective);
+
+                if !is_none {
+                    let indx = prev_exp.unwrap().index;
+                    self.nodes[indx].cost = new_cost;
+                    just_searched.push(indx);
+                } else {
+                    just_searched.push(self.new_child(i, new_pos, new_cost, weight));
+                }
+
+                //println!("{} | {}", Arena::heuristic(new_pos, *objective), new_cost);
                 //println!("{:?} {:?}", steps as f32 * 20.0, heuristic);
 
-                just_searched.push(self.new_child(i, new_pos, weight));
+                //println!("{:?}", weight);
 
                 if &new_pos == objective {
                     self.found_objective = Some(self.nodes.len() - 1);
@@ -272,7 +298,12 @@ impl Arena {
             }
         }
 
-        just_searched.into_iter().for_each(|js| self.open.push(js));
+        just_searched.into_iter().for_each(|js| {
+            if !self.open.contains(&js) {
+                self.open.push(js)
+            }
+        });
+        // }
 
         false
     }
@@ -297,7 +328,7 @@ impl Arena {
     }
 
     pub fn smart_reset(&mut self, placed: (f32, f32)) -> bool {
-        if self.exists(placed) {
+        if self.exists(placed).is_some() {
             self.reset();
             true
         } else {
@@ -306,7 +337,7 @@ impl Arena {
     }
 
     pub fn i_smart_reset(&mut self, placed: (f32, f32)) -> bool {
-        if !self.exists(placed) {
+        if self.exists(placed).is_none() {
             self.reset();
             true
         } else {
@@ -314,8 +345,8 @@ impl Arena {
         }
     }
 
-    pub fn exists(&self, pos: (f32, f32)) -> bool {
-        self.nodes.iter().any(|n| n.pos == pos)
+    pub fn exists(&self, pos: (f32, f32)) -> Option<&Node> {
+        self.nodes.iter().find(|n| n.pos == pos)
     }
 
     // pub fn manhattan_distance(&self, pos: (f32, f32)) -> f32 {
@@ -345,7 +376,8 @@ impl Arena {
 struct Node {
     parent: Option<usize>,
     index: usize,
-    pub weight: f32,
+    pub cost: f32,
+    pub priority: f32,
     pub pos: (f32, f32),
 }
 
@@ -433,7 +465,13 @@ async fn main() {
 
         if !arena.found_objective.is_some() {
             // arena.breadth_first_search(&walls, &objective, &(instant || !&visualize));
-            arena.a_search_star(&walls, &objective, &(instant || !&visualize));
+
+            // arena.a_search_star(&walls, &objective, &true);
+            // if instant {
+            arena.breadth_first_search(&walls, &objective, &false);
+            // } else {
+            //     arena.a_search_star(&walls, &objective, &false);
+            // }
         }
 
         if is_key_pressed(KeyCode::O) {
@@ -485,245 +523,3 @@ async fn main() {
         next_frame().await
     }
 }
-
-// Direction::all()
-//     .filter_map(|direction| {
-//         let (mut dx, mut dy) = (0.0_f32, 0.0_f32);
-//         let node = &self.nodes[index];
-//         match direction {
-//             &Direction::Up => dy = -SQUARE_SCALE,
-//             &Direction::Down => dy = SQUARE_SCALE,
-//             &Direction::Left => dx = -SQUARE_SCALE,
-//             &Direction::Right => dx = SQUARE_SCALE,
-//         };
-
-//         let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-
-//         if !self.nodes.iter().any(|n| n.pos == new_pos)
-//             && !walls.walls.iter().any(|w| w == &new_pos)
-//         {
-//             if &new_pos == objective {
-//                 self.on_objective = Some(self.nodes.len());
-//             }
-//             Some(self.new_child(Some(node.index), new_pos))
-//         } else {
-//             None
-//         }
-//     })
-//     .collect()
-
-// // loop {
-// let mut local_search = Vec::new();
-// for ls in last_searched.iter() {
-//     if !arena.on_objective.is_some() {
-//         arena
-//             .breadth_first_search(*ls, &walls, &objective)
-//             .into_iter()
-//             .for_each(|s| local_search.push(s))
-//     }
-// }
-
-// last_searched = local_search;
-
-// let found: usize;
-
-// let mut dirs: Vec<((f32, f32), f32)> = Vec::new();
-// for (dx, dy) in Direction::delta_dirs() {
-//     let node = &self.nodes[lowest_node];
-//     let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-
-//     if !self.exists(new_pos) && !walls.exists(&new_pos) {
-//         let final_weight = Arena::heuristic(*objective, new_pos);
-
-//         dirs.push((new_pos, final_weight));
-//         //                    just_searched.push(self.new_child(*i, new_pos, final_weight));
-
-//         if &new_pos == objective {
-//             found = self.new_child(lowest_node, new_pos, final_weight);
-//             self.open.push(found);
-//             //just_searched.push(self.new_child(*i, new_pos, final_weight));
-//             self.found_objective = Some(self.nodes.len() - 1);
-//             //found = true;
-//             return true;
-//         }
-//     }
-// }
-
-// if !dirs.is_empty() {
-//     let min = dirs
-//         .iter()
-//         .min_by(|d1, d2| d1.1.partial_cmp(&d2.1).unwrap())
-//         .unwrap();
-
-//     println!("{:?}", &min);
-
-//     found = self.new_child(lowest_node, min.0, min.1);
-//     self.open.push(found);
-// }
-
-// pub fn astar1(&mut self, walls: &Walls, objective: &(f32, f32), _instant: &bool) -> bool {
-//     // if *instant {
-//     //     loop {
-//     //         if self.breadth_first_search(walls, objective, &false) {
-//     //             return true;
-//     //         }
-//     //     }
-//     // }
-
-//     let mut just_searched = Vec::new();
-//     let mut found = false;
-
-//     // let i = self
-//     //     .searched
-//     //     .clone()
-//     //     .into_iter()
-//     //     .min_by(|i1, i2| {
-//     //         self.nodes[*i1]
-//     //             .weight
-//     //             .partial_cmp(&self.nodes[*i2].weight)
-//     //             .unwrap()
-//     //     })
-//     //     .unwrap_or(self.nodes.len() - 1);
-
-//     // for (dx, dy) in Direction::delta_dirs() {
-//     //     let node = &self.nodes[i];
-//     //     let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-
-//     //     if !self.exists(new_pos) && !walls.exists(&new_pos) {
-//     //         let mut weight: usize = 0;
-//     //         node.total_steps(&mut weight, self);
-//     //         let weight = weight as f32 + Arena::heuristic(new_pos, *objective);
-
-//     //         just_searched.push(self.new_child(i, new_pos, weight));
-
-//     //         if &new_pos == objective {
-//     //             self.found_objective = Some(self.nodes.len() - 1);
-//     //             found = true;
-//     //             break;
-//     //         }
-//     //     }
-//     // }
-
-//     let mut sorted = self.open.clone();
-//     sorted.sort_by(|i1, i2| {
-//         self.nodes[*i1]
-//             .weight
-//             .partial_cmp(&self.nodes[*i2].weight)
-//             .unwrap()
-//     });
-
-//     println!("{:?}", &sorted);
-
-//     let mut found = false;
-//     'outer: for i in &sorted {
-//         //let mut succeeded = false;
-
-//         let mut dirs: Vec<((f32, f32), f32)> = Vec::new();
-//         for (dx, dy) in Direction::delta_dirs() {
-//             let node = &self.nodes[*i];
-//             let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-
-//             if !self.exists(new_pos) && !walls.exists(&new_pos) {
-//                 //let mut weight: usize = 0;
-//                 //node.total_steps(&mut weight, self);
-//                 //(weight as f32) * SQUARE_SCALE +
-//                 let final_weight = Arena::heuristic(*objective, new_pos);
-
-//                 dirs.push((new_pos, final_weight));
-//                 //                    just_searched.push(self.new_child(*i, new_pos, final_weight));
-
-//                 if &new_pos == objective {
-//                     just_searched.push(self.new_child(*i, new_pos, final_weight));
-//                     self.found_objective = Some(self.nodes.len() - 1);
-//                     found = true;
-//                     break 'outer;
-//                 }
-
-//                 //succeeded = true;
-//             }
-//         }
-
-//         if !dirs.is_empty() {
-//             let min = dirs
-//                 .iter()
-//                 .min_by(|d1, d2| d1.1.partial_cmp(&d2.1).unwrap())
-//                 .unwrap();
-
-//             println!("{:?}", &min);
-
-//             just_searched.push(self.new_child(*i, min.0, min.1));
-//             break 'outer;
-//         }
-//         // if succeeded {
-//         // break;
-//         // }
-//         //break 'outer;
-//     }
-//     // /self.searched = just_searched;
-//     just_searched.iter().for_each(|js| self.open.push(*js));
-
-//     found // just_searched.is_empty() || found
-// }
-
-//pub fn a_search_star(
-//     &mut self,
-//     walls: &Walls,
-//     objective: &(f32, f32),
-//     _instant: &bool,
-// ) -> bool {
-//     let i = self
-//         .open
-//         .clone()
-//         .into_iter()
-//         .min_by(|n1, n2| {
-//             self.nodes[*n1]
-//                 .weight
-//                 .partial_cmp(&self.nodes[*n2].weight)
-//                 .unwrap()
-//         })
-//         .unwrap();
-
-//     let p = self.open.iter().position(|u| u == &i).unwrap();
-//     self.open.remove(p);
-
-//     let mut just_searched: Vec<usize> = Vec::new();
-
-//     let mut dirs: Vec<(f32, (f32, f32))> = Vec::new();
-//     for (dx, dy) in Direction::delta_dirs() {
-//         let node = &self.nodes[i];
-//         let new_pos = (node.pos.0 + dx, node.pos.1 + dy);
-//         if !self.exists(new_pos) && !walls.exists(&new_pos) {
-//             // let mut steps = 0;
-//             // node.total_steps(&mut steps, self);
-//             // let weight = (steps + 1) as f32 + Arena::heuristic(*objective, new_pos);
-
-//             let weight =
-//                 self.distance_from_start(new_pos) + Arena::heuristic(*objective, new_pos);
-//             dirs.push((weight, new_pos));
-
-//             if &new_pos == objective {
-//                 just_searched.push(self.new_child(i, new_pos, weight));
-//                 self.found_objective = Some(self.nodes.len() - 1);
-//                 return true;
-//             }
-//         }
-//     }
-
-//     if !dirs.is_empty() {
-//         dirs.sort_by(|d1, d2| d1.0.partial_cmp(&d2.0).unwrap());
-//         //println!("{:?}", dirs);
-//         let mut last = dirs[0].0;
-//         for dir in dirs {
-//             if dir.0 != last {
-//                 break;
-//             }
-
-//             last = dir.0;
-//             just_searched.push(self.new_child(i, dir.1, dir.0));
-//         }
-//     }
-
-//     just_searched.into_iter().for_each(|js| self.open.push(js));
-
-//     false
-// }
